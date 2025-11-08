7fdd280f0b3f7bcecacdebbcda1e8a3c0909ba82
TK <leandro.kinoshita@quintoandar.com.br>
2025-11-08 15:22:36 -0300

    fix(ConvModelV2): move `BatchNorm` before `gelu` activation function

--- src/dlfb/dna/model.py
+++ src/dlfb/dna/model.py
@@ -79,8 +79,8 @@ class ConvModelV2(nn.Module):
     x = nn.Conv(
       features=self.conv_filters, kernel_size=self.kernel_size, padding="SAME"
     )(x)
-    x = nn.gelu(x)
     x = nn.BatchNorm(use_running_average=not is_training)(x)
+    x = nn.gelu(x)
     x = nn.max_pool(x, window_shape=(2,), strides=(2,))
 
     # Flatten the values before passing them to the dense layers.
